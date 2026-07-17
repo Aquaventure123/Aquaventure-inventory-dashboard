89f8cce8bee92364c23e65e3a908906c6c6b911b
Aquaventure123 <info@aquaventure.in>
2026-07-17 12:55:06 +0530

    Updated dashboard

--- js/users.js
+++ js/users.js
@@ -6,9 +6,14 @@
 // ================================================================
 
 const USERS = [
-  { username: "Santosh", password: "Aqv@123", displayName: "Santosh" },
-  { username: "wendy", password: "Wendy@123", displayName: "Wendy" },
-  { username: "kevin", password: "Kevin@123", displayName: "Kevin" },
-  { username: "jamison", password: "Jamison@123", displayName: "Jamison" },
+  { username: "Santosh", password: "12341", displayName: "Santosh" },
+  { username: "Kapil", password: "12340", displayName: "Kapil Kewlani" },
+  { username: "Gunjan", password: "12342", displayName: "Gunjan Verma" },
+  { username: "Nimish", password: "12343", displayName: "Nimish gautam" },
+  { username: "Santosh sah", password: "12344", displayName: "Santosh Sah" },
+  { username: "Rajeev", password: "12345", displayName: "Rajeev Nag" },
+  { username: "Deepak", password: "12346", displayName: "Deepak Jeengar" },
+  { username: "Harshit", password: "12347", displayName: "Harshit Naval" },
+  { username: "Ajay", password: "12348", displayName: "Ajay Verma" },
   { username: "william", password: "William@123", displayName: "William" },
 ];
